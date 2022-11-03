e28880ebd99d576b571ca5786e4b4f805504d863
mikailov038 <mikailov038@gmail.com>
2022-11-04 02:05:12 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package ru.kata.spring.boot_security.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    private final SuccessUserHandler successUserHandler;
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+
+    @Autowired
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, PasswordEncoder passwordEncoder, UserService userService) {
+        this.successUserHandler = successUserHandler;
+        this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
+    }
+
+    @Bean
+    public AuthenticationManager getAuthenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder)
+                .and().build();
+    }
+
+    @Bean
+    public SecurityFilterChain authorizeAllRequests(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().successHandler(successUserHandler)
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+        return http.build();
+    }
+
+}
